723fcb7a4aeca78bd87783a6f5502c315812d281
yan-man <yan.man45@gmail.com>
2022-08-19 23:44:27 -0500

    set JobManager contract;

--- test/unit/KeepersManager/KeepersManager.spec.ts
+++ test/unit/KeepersManager/KeepersManager.spec.ts
@@ -4,10 +4,13 @@ import { ethers } from "hardhat";
 
 export const UnitTest = (): void => {
   describe("Deployment", function () {
-    it("Should set the right owner", async function () {
-      // expect(await this.dCAManager.owner()).to.equal(this.signers[0].address);
+    // it("Should set the right owner", async function () {
+    //   // expect(await this.dCAManager.owner()).to.equal(this.signers[0].address);
+    // });
+    it("Should set JobManager address", async function () {
+      await expect(this.keepersManager.setJobManager(this.signers[1].address))
+        .to.not.be.reverted;
     });
-    // it("Should set the right owner", async function () {});
     // it("Should set the right owner", async function () {});
     // it("Should set the right owner", async function () {});
     // it("Should set the right owner", async function () {});
