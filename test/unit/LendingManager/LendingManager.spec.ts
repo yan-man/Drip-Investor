dab9819a4c827384feebf19f851e746a96553a19
yan-man <yan.man45@gmail.com>
2022-08-22 02:29:58 -0500

    remove comments;

--- test/unit/LendingManager/LendingManager.spec.ts
+++ test/unit/LendingManager/LendingManager.spec.ts
@@ -63,20 +63,4 @@ export const UnitTest = (): void => {
       });
     });
   });
-  describe("Lending Manager", function () {
-    // it("Should allow setting the Deposit token address", async function () {});
-    // it("Should set the lending pool and  provider on construction", async function () {});
-    // it("Should not allow deposit if not initialized", async function () {});
-    // it("Should allow setting the token address (ie usdc)", async function () {});
-    // it("Should revert if necessary addresses aren't initialized", async function () {});
-
-    // it("Should set the lending pool and provider on construction", async function () {});
-    // it("Should allow ", async function () {});
-    // it("Should set the lending pool and  provider on construction", async function () {});
-    // it("Should set the lending pool and  provider on construction", async function () {});
-    // it("Should set the lending pool and  provider on construction", async function () {});
-    describe("Events", function () {
-      it("Should emit event LogDeposit when deposited into lender", async function () {});
-    });
-  });
 };
