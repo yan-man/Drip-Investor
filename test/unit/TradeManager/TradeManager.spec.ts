d2ede0f6ab59ea1f5869a617f5e5d39dd8c0b294
yan-man <yan.man45@gmail.com>
2022-08-22 01:22:20 -0500

    resolve all tests;

--- test/unit/TradeManager/TradeManager.spec.ts
+++ test/unit/TradeManager/TradeManager.spec.ts
@@ -58,6 +58,24 @@ export const UnitTest = (): void => {
       });
 
       describe(`ExecuteJob - DCA via swap`, async function () {
+        beforeEach(`...set up mocks`, async function () {
+          this._investmentAmount = 100;
+          this._user = this.signers[4].address;
+          this._jobId = 0;
+
+          await this.mocks.mockJobManager.mock.isValidId.returns(true);
+          await this.mocks.mockDCAManager.mock.s_userJobs
+            .withArgs(this._user, this._jobId)
+            .returns(this._investmentAmount);
+          await this.mocks.mockJobManager.mock.s_jobs.returns(
+            ethers.BigNumber.from(this._jobId), // jobId
+            this._user, // owner address
+            ethers.BigNumber.from("0"), // frequencyOptionId
+            true, // isActive
+            ethers.BigNumber.from("1661074126"), // startTime
+            ethers.BigNumber.from(this._investmentAmount) // investmentAmount
+          );
+        });
         it("Should not allow swap for invalid Id", async function () {
           await this.mocks.mockJobManager.mock.isValidId.returns(false);
           await expect(
@@ -67,32 +85,17 @@ export const UnitTest = (): void => {
             `TradeManager__InvalidId`
           );
         });
-        it("Should allow swap for valid jobId", async function () {
-          const _investmentAmount = 100;
-          const _user = this.signers[4].address;
-          const _jobId = 0;
-
-          await this.mocks.mockJobManager.mock.isValidId.returns(true);
-          await this.mocks.mockDCAManager.mock.s_userJobs
-            .withArgs(_user, _jobId)
-            .returns(_investmentAmount);
-          await this.mocks.mockJobManager.mock.s_jobs.returns(
-            ethers.BigNumber.from(_jobId), // jobId
-            _user, // owner address
-            ethers.BigNumber.from("0"), // frequencyOptionId
-            true, // isActive
-            ethers.BigNumber.from("1661074126"), // startTime
-            ethers.BigNumber.from(_investmentAmount) // investmentAmount
-          );
-          await expect(this.tradeManager.executeJob(0)).to.be.not.reverted;
-        });
         it("Should revert if insufficient deposits", async function () {
-          await this.mocks.mockJobManager.mock.isValidId.returns(true);
+          await this.mocks.mockDCAManager.mock.s_userJobs
+            .withArgs(this._user, this._jobId)
+            .returns(this._investmentAmount - 1);
+          await expect(this.tradeManager.executeJob(0)).to.be.reverted;
+        });
+        it("Should allow swap for valid jobId", async function () {
           await expect(this.tradeManager.executeJob(0)).to.be.not.reverted;
         });
         describe(`Events`, async function () {
           it("Should emit on successful executedJob", async function () {
-            await this.mocks.mockJobManager.mock.isValidId.returns(true);
             await expect(this.tradeManager.executeJob(0)).to.emit(
               this.tradeManager,
               `LogExecuteJob`
